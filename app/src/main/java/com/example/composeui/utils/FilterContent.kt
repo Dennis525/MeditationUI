f64af2558280cb777bc48dcb6a6acfdb12e99195
Dennis Okumu <dennisokumu72@yahoo.com>
2022-08-03 00:15:19 +0300

    Starter Code

---
+++ app/src/main/java/com/example/composeui/utils/FilterContent.kt
@@ -0,0 +1,9 @@
+package com.example.composeui.utils
+
+import androidx.compose.ui.graphics.Color
+
+data class FilterContent(
+    val contentColor: Color,
+    val backgroundColor: Color,
+    val filterText: String
+)
