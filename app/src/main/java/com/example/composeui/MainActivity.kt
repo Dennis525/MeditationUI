2c6fb2a309e27d48540ca1298c584c7f747c3f04
Dennis Okumu <dennisokumu72@yahoo.com>
2022-08-03 09:10:12 +0300

    ProfileComponent

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -3,41 +3,85 @@ package com.example.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeui.ui.theme.ComposeUITheme
+import com.example.composeui.ui.theme.Grey
+import com.example.composeui.ui.theme.nunitoLight
+import com.example.composeui.ui.theme.nunitoMedium
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                   modifier = Modifier
+                       .background(Grey)
+                       .fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    HeaderProfileComponent()
                 }
+
             }
+        }
+    }
+    @Composable
+    fun HeaderProfileComponent() {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 15.dp, end = 15.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profilepicture),
+                    contentDescription = "Profile picture",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                ) {
+                    Text(
+                        text = "Welcome Back",
+                        fontFamily = nunitoLight,
+                        fontSize = 14.sp,
+                        textAlign = TextAlign.Start
+                    )
+                    Text(
+                        text = "Dennis Odhiambo",
+                        fontFamily = nunitoMedium,
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Start
+                    )
+
+                }
+
+            }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeUITheme {
-        Greeting("Android")
-    }
-}
